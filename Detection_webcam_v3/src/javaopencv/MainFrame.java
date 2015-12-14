25e070304990c016e490da0f48a9160ad22e3ccb
tof360 <chris_1232@hotmail.com>
2015-12-15 00:43:27 +0100

    Authentification par choix des carrés

--- Detection_webcam_v3/src/javaopencv/MainFrame.java
+++ Detection_webcam_v3/src/javaopencv/MainFrame.java
@@ -51,33 +51,28 @@ public class MainFrame extends javax.swing.JFrame
   private int savedelay = 0;
   String currentDir = "";
   String detectionsDir = "detections";
-    private Object jTextField1;
-
+  private Object jTextField1;
   
+  public Rect rectA = new Rect();
+  public Rect rectB = new Rect();
+  public Rect rectC = new Rect();
+  public Rect rectD = new Rect();
+  public static int rectWidth = 80;
+  public static int rectHeight = 60;
   
+  public int cptRect=0;
+  public int numeroRect[] = new int[3];
   
-
   public MainFrame()
   {
     initComponents();
     image = new ImagePanel(new ImageIcon("figs/320x240.gif").getImage());
     jPanelSource1.add(image, BorderLayout.CENTER);
     
-
-    /*
-    currentDir = Paths.get(".").toAbsolutePath().normalize().toString();
-    detectionsDir = currentDir + File.separator + detectionsDir;
-    // new java.io.File( "." ).getCanonicalPath();
-    //System.out.println("Current dir: " + currentDir);
-    //System.out.println("Detections dir: " + detectionsDir);
-    jTextFieldSaveLocation.setText(detectionsDir);
-    */
   }
 
   private void start()
   {
-    //System.out.println("You clicked the start button!");
-
     if(!begin)
     {
       int sourcen = Integer.parseInt(jTextFieldSource1.getText());
@@ -94,19 +89,17 @@ public class MainFrame extends javax.swing.JFrame
       }
     }
   }
+  
+  public void reinitialise() {
+      this.cptRect = 0;
+  }
 
   private void stop()
   {
-    //System.out.println("You clicked the stop button!");
-
-    if(begin)
-    {
-      try
-      {
+    if(begin) {
+      try{
         Thread.sleep(500);
-      }
-      catch(Exception ex)
-      {
+      } catch(Exception ex){
       }
       video.release();
       begin = false;
@@ -122,35 +115,32 @@ public class MainFrame extends javax.swing.JFrame
   }
 
   int findBiggestContour(List<MatOfPoint> contours) 
-	{
-		int idx = -1;
-		int cNum = 0;
-		
-		for (int i = 0; i < contours.size(); i++)
-		{
-			int curNum = contours.get(i).toList().size();
-			if (curNum > cNum) {
-				idx = i;
-				cNum = curNum;
-			}
-		}
-		
-                return idx;
-	}
+    {
+        int idx = -1;
+        int cNum = 0;
+
+        for (int i = 0; i < contours.size(); i++)
+        {
+                int curNum = contours.get(i).toList().size();
+                if (curNum > cNum) {
+                        idx = i;
+                        cNum = curNum;
+                }
+        }		
+        return idx;
+    }
   
-  boolean isClosedToBoundary(Point pt, Mat img)
-	{
-		int margin = 5;
-		if ((pt.x > margin) && (pt.y > margin) && 
-				(pt.x < img.cols()-margin) &&
-				(pt.y < img.rows()-margin)) {
-			return false;
-		}
-		
-		return true;
-	}
   
-  public ArrayList<Rect>  detection_contours(Mat inmat, Mat outmat)
+  public boolean isInRectangle(Rect source, Point a) {
+      Point debut = new Point (source.x,source.y);
+      Point fin = new Point ((source.x + source.width),(source.y + source.height));
+      
+      if ( (a.x > debut.x) && (a.x < fin.x) && (a.y > debut.y) && (a.y < fin.y) ) {
+          return true;
+      } else return false;
+  }
+  
+  public void  detection_contours(Mat inmat, Mat outmat)
   {
     Mat v = new Mat();
     Mat vv = outmat.clone();
@@ -159,19 +149,18 @@ public class MainFrame extends javax.swing.JFrame
     List<MatOfPoint> hullP = new ArrayList<MatOfPoint>();
     MatOfInt4 defects = new MatOfInt4();
     Rect boundingRect = null;
+    double maxArea = 100;
+    int maxAreaIdx;
+    Rect r;
+    ArrayList<Rect> rect_array = new ArrayList();
     
-    
+    // Trouve tous les contours
     Imgproc.findContours(vv, contours, v, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
     int key = findBiggestContour(contours);
     if (key!= 0){
         Imgproc.drawContours(inmat, contours, key, new Scalar(0, 0, 255)); 
     }
         
-        double maxArea = 100;
-    int maxAreaIdx;
-    Rect r;
-    ArrayList<Rect> rect_array = new ArrayList();
-
     for(int idx = 0; idx < contours.size(); idx++)
     {
       Mat contour = contours.get(idx);
@@ -185,63 +174,174 @@ public class MainFrame extends javax.swing.JFrame
         Imgproc.drawContours(inmat, contours, maxAreaIdx, new Scalar(0, 0, 255));
       }
     }
+    
+    // Dessine les rectangle autour des contours
+    if(rect_array.size() > 0) {
+      Iterator<Rect> it2 = rect_array.iterator();
+      while(it2.hasNext()){
+        Rect obj = it2.next();
+        Core.rectangle(currentFrame, obj.br(), obj.tl(),
+          new Scalar(0, 255, 0), 1);
+      }
+    }
 
    
-        Imgproc.convexHull(contours.get(key), hullI, false);
-        
-        if (hullI != null) {
-            hullP.clear();
-            for (int i = 0; contours.size() >= i; i++)
-            hullP.add(new MatOfPoint());
+    Imgproc.convexHull(contours.get(key), hullI, false);
 
-        int[] cId = hullI.toArray();
-	List<Point> lp = new ArrayList<Point>();
-	Point[] contourPts = contours.get(key).toArray();
-        int cpt =0;
-        for (int i = 0; i < cId.length; i++)
-            {
-                lp.add(contourPts[cId[i]]);
-                Core.circle(inmat, contourPts[cId[i]], 2, new Scalar(241, 247, 45), -3);
-                cpt++;
-        }
+    if (hullI != null) {
+        hullP.clear();
+        for (int i = 0; contours.size() >= i; i++)
+        hullP.add(new MatOfPoint());
 
-        //hg.hullP.get(hg.cMaxId) returns the locations of the points in the convex hull of the hand
-        hullP.get(key).fromList(lp);
-        lp.clear();
-        
+    int[] cId = hullI.toArray();
+    List<Point> lp = new ArrayList<Point>();
+    Point[] contourPts = contours.get(key).toArray();
+    
+    
+    int findRectA = 0;
+    int findRectB = 0;
+    int findRectC = 0;
+    int findRectD = 0;
+    
+    
+    
+    for (int i = 0; i < cId.length; i++)
+        {
+            lp.add(contourPts[cId[i]]);
+            Core.circle(inmat, contourPts[cId[i]], 2, new Scalar(241, 247, 45), -3);
+
+            if (isInRectangle(rectA,contourPts[cId[i]])) 
+                findRectA++;
+            
+            if (isInRectangle(rectB,contourPts[cId[i]])) 
+                findRectB++;
+            
+            if (isInRectangle(rectC,contourPts[cId[i]])) 
+                findRectC++;
+            
+            if (isInRectangle(rectD,contourPts[cId[i]])) 
+                findRectD++;
+    }
+
+    // TODO : Ajout
+    
+    if (findRectA>=5) {
+        if (cptRect==0) {
+            numeroRect[cptRect] = 1;
+            cptRect++;
+            System.out.println("Haut gauche"); 
+        } else {
+            if (numeroRect[cptRect-1] != 1) {
+                numeroRect[cptRect] = 1;
+                cptRect++;
+                System.out.println("Haut gauche"); 
+           }    
         }
+    }
+    
+    if (findRectB>=5) {
+        if (cptRect==0) {
+            numeroRect[cptRect] = 2;
+            cptRect++;
+            System.out.println("Bas gauche"); 
+        } else {
+            if (numeroRect[cptRect-1] != 2) {
+                numeroRect[cptRect] = 2;
+                cptRect++;
+                System.out.println("Bas gauche"); 
+           }    
+        }
+    }
+    
+    if (findRectC>=5) {
+        if (cptRect==0) {
+            numeroRect[cptRect] = 3;
+            cptRect++;
+            System.out.println("Haut droite"); 
+        } else {
+            if (numeroRect[cptRect-1] != 3) {
+                numeroRect[cptRect] = 3;
+                cptRect++;
+                System.out.println("Haut droite"); 
+           }    
+        }
+    }
+    
+    if (findRectD>=5) {
+        if (cptRect==0) {
+            numeroRect[cptRect] = 4;
+            cptRect++;
+            System.out.println("Bas droite"); 
+        } else {
+            if (numeroRect[cptRect-1] != 4) {
+                numeroRect[cptRect] = 4;
+                cptRect++;
+                 System.out.println("Bas droite"); 
+           }    
+        }
+    }
+    
+    
+    
+    // Si on a sélectionné 3 fenètres
+    if (cptRect == 3) {
+       if ((numeroRect[0] == 1) && (numeroRect[1] == 4) && (numeroRect[2] == 2))
+          this.jTextField2.setText("Authenticated");
+       
+       cptRect=0;
+    }
+
+
+    //hg.hullP.get(hg.cMaxId) returns the locations of the points in the convex hull of the hand
+    hullP.get(key).fromList(lp);
+    lp.clear();
+
+    }
          
-         v.release();
-    return rect_array;
+       
+    
+  }
+  
+  public void initialiseRectangle() {
+    rectA.x = 10;     rectA.y = 10;
+    rectB.x = 10;     rectB.y = 170;
+    rectC.x = 230;     rectC.y = 10;
+    rectD.x = 230;     rectD.y = 170;
+
+    rectA.height = MainFrame.rectHeight;
+    rectB.height = MainFrame.rectHeight;
+    rectC.height = MainFrame.rectHeight;
+    rectD.height = MainFrame.rectHeight;
+
+    rectA.width = MainFrame.rectWidth;
+    rectB.width = MainFrame.rectWidth;
+    rectC.width = MainFrame.rectWidth;
+    rectD.width = MainFrame.rectWidth;
   }
     
-      
-   
-  
- 
-
-        
-        /*     
-        Imgproc.convexityDefects(contours.get(key), hullI, defects);
-
-        Scalar[] mColorsRGB = null;
-        Core.rectangle(vv, boundingRect.tl(), boundingRect.br(), mColorsRGB[1], 2);
-        Imgproc.drawContours(vv, hullP, key, mColorsRGB[2]);
-        */
   class CaptureThread extends Thread
   {
     @Override
     public void run()
     {
+      cptRect = 0;
       if(video.isOpened())
       {
         while(begin == true)
         {
-          //video.read(frameaux);
-          video.retrieve(frameaux);
-          Imgproc.resize(frameaux, frame, frame.size());
-          frame.copyTo(currentFrame);
+            //video.read(frameaux);
+            video.retrieve(frameaux);
+            Imgproc.resize(frameaux, frame, frame.size());
+            frame.copyTo(currentFrame);
+            
+            initialiseRectangle();
           
+          
+          /* a.x = 10;
+           a.y = 10;
+           a.height = 80;
+           a.width = 60; */
+            
         
           if(jCheckBoxMotionDetection.isSelected())
           {
@@ -267,30 +367,23 @@ public class MainFrame extends javax.swing.JFrame
             Imgproc.threshold(processedFrame, processedFrame, threshold, 255, Imgproc.THRESH_BINARY);
 
             
-            
-            
-            ArrayList<Rect> array = detection_contours(currentFrame, processedFrame);
-            
-            
-            
-            if(array.size() > 0)
-            {
-              Iterator<Rect> it2 = array.iterator();
-              while(it2.hasNext())
-              {
-                Rect obj = it2.next();
-                Core.rectangle(currentFrame, obj.br(), obj.tl(),
-                  new Scalar(0, 255, 0), 1);
-              }
-            }
-       
+           
+             detection_contours(currentFrame, processedFrame);
+
             } 
            
             //currentFrame.copyTo(processedFrame);
+           
           
-         
-          currentFrame.copyTo(processedFrame);
-
+          
+        // Dessine les rectangles d'authentifications       
+        Core.rectangle(currentFrame, rectA.br(), rectA.tl(), new Scalar(0, 255, 255), 2); 
+        Core.rectangle(currentFrame, rectB.br(), rectB.tl(), new Scalar(0, 255, 0), 2); 
+        Core.rectangle(currentFrame, rectC.br(), rectC.tl(), new Scalar(255, 255, 0), 2); 
+        Core.rectangle(currentFrame, rectD.br(), rectD.tl(), new Scalar(255, 0, 0), 2); 
+        
+        currentFrame.copyTo(processedFrame);
+    
           Highgui.imencode(".jpg", processedFrame, matOfByte);
           byte[] byteArray = matOfByte.toArray();
 
@@ -321,6 +414,7 @@ public class MainFrame extends javax.swing.JFrame
     }
   }
 
+
   /**
    * This method is called from within the constructor to initialize the form.
    * WARNING: Do NOT modify this code. The content of this method is always
@@ -339,6 +433,8 @@ public class MainFrame extends javax.swing.JFrame
         jSliderThreshold = new javax.swing.JSlider();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java OpenCV Webcam");
@@ -386,17 +482,27 @@ public class MainFrame extends javax.swing.JFrame
 
         jLabel2.setText("(zero for local webcamera)");
 
+        jTextField2.setEditable(false);
+        jTextField2.setText("not authenticated");
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Reinitialize");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(63, 63, 63)
-                        .addComponent(jButtonStart, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButtonStop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
@@ -413,7 +519,16 @@ public class MainFrame extends javax.swing.JFrame
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                     .addComponent(jTextFieldSource1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jLabel2))))))
+                                    .addComponent(jLabel2)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(63, 63, 63)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButtonStart, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButtonStop, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(27, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -432,11 +547,15 @@ public class MainFrame extends javax.swing.JFrame
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jCheckBoxMotionDetection)
                         .addComponent(jLabel1)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButtonStart)
                     .addComponent(jButtonStop))
-                .addGap(68, 68, 68))
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addContainerGap())
         );
 
         pack();
@@ -451,6 +570,14 @@ public class MainFrame extends javax.swing.JFrame
     {//GEN-HEADEREND:event_jButtonStopActionPerformed
       stop();
     }//GEN-LAST:event_jButtonStopActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       reinitialise();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
   public static void main(String args[])
   {
@@ -501,6 +628,7 @@ public class MainFrame extends javax.swing.JFrame
   }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonStart;
     private javax.swing.JButton jButtonStop;
     private javax.swing.JCheckBox jCheckBoxMotionDetection;
@@ -509,6 +637,7 @@ public class MainFrame extends javax.swing.JFrame
     private javax.swing.JLabel jLabelSource1;
     private javax.swing.JPanel jPanelSource1;
     private javax.swing.JSlider jSliderThreshold;
+    private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextFieldSource1;
     // End of variables declaration//GEN-END:variables
 }
